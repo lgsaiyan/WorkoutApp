1cbb367b0b313a2f82ceec980bf7de68118835c2
lgsaiyan <philovon@Live.com>
2020-10-31 11:10:54 -0500

    another attempt

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -11,4 +11,17 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(port);
+app.listen(port); */
+
+
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
